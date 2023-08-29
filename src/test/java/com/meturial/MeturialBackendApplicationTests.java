5b4feb65b2fcbb8920bceabedfbf8df8328d14ea
soyeonkim0227 <soyeonkim0227@naver.com>
2023-08-29 23:14:25 +0900

    ⚙️ :: 프로젝트 생성

---
+++ src/test/java/com/meturial/MeturialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meturial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeturialBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
