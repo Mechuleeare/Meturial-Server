cc08ce9833675a171f1267c732640c4199b2529c
soyeonkim0227 <soyeonkim0227@naver.com>
2023-08-30 14:34:05 +0900

    ⚙️ :: Exception 설정 추가

---
+++ src/main/java/com/meturial/global/error/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.meturial.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+}
