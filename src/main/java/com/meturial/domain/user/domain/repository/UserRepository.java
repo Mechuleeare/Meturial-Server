49545aab396d0d4cfbb375459342262fa57ac46b
soyeonkim0227 <soyeonkim0227@naver.com>
2023-09-12 10:00:24 +0900

    💡 :: UserRepository 메서드 추가

--- src/main/java/com/meturial/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/meturial/domain/user/domain/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.meturial.domain.user.domain.repository;
 import com.meturial.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
 }
