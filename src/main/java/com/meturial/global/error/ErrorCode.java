2671548c806bd210ef625a3747eddcbd793a14e0
soyeonkim0227 <soyeonkim0227@naver.com>
2023-09-14 11:51:22 +0900

    💡 :: ErrorCode 수정

--- src/main/java/com/meturial/global/error/ErrorCode.java
+++ src/main/java/com/meturial/global/error/ErrorCode.java
@@ -10,14 +10,14 @@ public enum ErrorCode {
     EXPIRED_JWT(401, "Expired Jwt"),
     INVALID_JWT(401, "Invalid Jwt"),
 
-    USER_NOT_FOUND(404, "User Not Found"),
-    USER_EXIST(409, "User Exist"),
-
     CODE_ALREADY_EXPIRED(401, "Code Already Expired"),
     EMAIL_NOT_CERTIFIED(401, "Email Not Certified"),
     CODE_NOT_CORRECT(401, "Code Not Correct"),
-    SEND_MESSAGE_FAILED(500, "Send Message Failed"),
 
+    USER_NOT_FOUND(404, "User Not Found"),
+    USER_EXIST(409, "User Exist"),
+
+    SEND_MESSAGE_FAILED(500, "Send Message Failed"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private final int status;
