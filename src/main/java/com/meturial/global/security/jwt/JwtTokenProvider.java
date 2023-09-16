8a5351a659f8377a8b52e69f00d4d1630a24f62b
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-16 14:11:41 +0900

    🌼 :: Token 발급 메소드 추가

--- src/main/java/com/meturial/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/meturial/global/security/jwt/JwtTokenProvider.java
@@ -2,6 +2,7 @@ package com.meturial.global.security.jwt;
 
 import com.meturial.domain.auth.domain.RefreshToken;
 import com.meturial.domain.auth.domain.repository.RefreshTokenRepository;
+import com.meturial.domain.auth.presentation.dto.response.TokenResponse;
 import com.meturial.global.exception.ExpiredJwtException;
 import com.meturial.global.exception.InvalidJwtException;
 import com.meturial.global.security.auth.AuthDetailsService;
@@ -15,6 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
@@ -24,6 +26,14 @@ public class JwtTokenProvider {
     private final JwtProperty jwtProperty;
     private final AuthDetailsService authDetailsService;
     private final RefreshTokenRepository refreshTokenRepository;
+
+    public TokenResponse getTokens(String email) {
+        return new TokenResponse(
+                generateAccessToken(email),
+                LocalDateTime.now().plusSeconds(jwtProperty.getAccessExpiration()),
+                generateRefreshToken(email)
+        );
+    }
 
     public String generateAccessToken(String email) {
         return generateToken(email, "access", jwtProperty.getAccessExpiration());
