fc34f8541ed8f38828ecf6fda3dd4098f4ac56c8
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-16 12:46:56 +0900

    💡 :: MenuNotification 엔티티 제거

--- src/main/java/com/meturial/domain/notification/domain/repository/MenuNotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meturial.domain.notification.domain.repository;
-
-import com.meturial.domain.notification.domain.MenuNotification;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface MenuNotificationRepository extends CrudRepository<MenuNotification, UUID> {
-}
