80cc4b85ac537a490578a8758bda578c101fce32
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-20 22:54:31 +0900

    🌼 :: 로그아웃 API 추가

--- src/main/java/com/meturial/domain/auth/presentation/AuthController.java
+++ src/main/java/com/meturial/domain/auth/presentation/AuthController.java
@@ -4,9 +4,12 @@ import com.meturial.domain.auth.presentation.dto.request.UserSignInRequest;
 import com.meturial.domain.auth.presentation.dto.response.TokenResponse;
 import com.meturial.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,5 +22,11 @@ public class AuthController {
     @PostMapping("/token")
     public TokenResponse signIn(@RequestBody UserSignInRequest request) {
         return authService.signIn(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/logout")
+    public void logOut() {
+        authService.logOut();
     }
 }
