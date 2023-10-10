59ab9086805171134a6c078fa38b8a1f5981ade1
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-10-10 15:16:55 +0900

    🌼 :: RedisConfig 비밀번호 설정

--- src/main/java/com/meturial/global/redis/RedisConfig.java
+++ src/main/java/com/meturial/global/redis/RedisConfig.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
@@ -18,15 +18,18 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
+    @Value("${spring.data.redis.password}")
+    private String password;
 
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(host, port);
+
+        boolean isNotBlankPassword = !password.isBlank();
+        if (isNotBlankPassword) {
+            redisConfiguration.setPassword(password);
+        }
+
+        return new LettuceConnectionFactory(redisConfiguration);
     }
 }
