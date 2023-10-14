13a38424b7c98950c08fdc6aa811818ee72e6ae8
soyeonkim0227 <soyeonkim0227@naver.com>
2023-10-14 19:00:19 +0900

    🌼 :: 찜 추가하기 API 생성

---
+++ src/main/java/com/meturial/domain/recipe/facade/RecipeFacade.java
@@ -0,0 +1,21 @@
+package com.meturial.domain.recipe.facade;
+
+import com.meturial.domain.recipe.domain.Recipe;
+import com.meturial.domain.recipe.domain.repository.RecipeRepository;
+import com.meturial.domain.recipe.exception.RecipeNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class RecipeFacade {
+
+    private final RecipeRepository recipeRepository;
+
+    public Recipe checkRecipe(UUID recipeId) {
+        return recipeRepository.findById(recipeId)
+                .orElseThrow(() -> RecipeNotFoundException.EXCEPTION);
+    }
+}
