f373dc49f636a38556e8a174c0cd53072b5ee7cb
soyeonkim0227 <soyeonkim0227@naver.com>
2023-10-14 20:10:29 +0900

    💡 :: ErrorCode 상태 코드 수정

--- src/main/java/com/meturial/global/error/ErrorCode.java
+++ src/main/java/com/meturial/global/error/ErrorCode.java
@@ -16,10 +16,10 @@ public enum ErrorCode {
     UN_AUTHORIZED(401, "Un Authorized"),
 
     USER_NOT_FOUND(404, "User Not Found"),
+    RECIPE_NOT_FOUND(404, "Recipe Not Found"),
     REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found"),
 
     USER_EXIST(409, "User Exist"),
-    RECIPE_NOT_FOUND(409, "Recipe Not Found"),
     CHOICE_RECIPE_EXIST(409, "Choice Recipe Exist"),
 
     SEND_MESSAGE_FAILED(500, "Send Message Failed"),
