eda804f683fd8e7625f15a3ae41211d35cfecb54
soyeonkim0227 <soyeonkim0227@naver.com>
2023-10-27 17:10:07 +0900

    🌼 :: Dto 추가

---
+++ src/main/java/com/meturial/domain/recipe/presentation/dto/response/QueryChoiceRecipeListResponse.java
@@ -0,0 +1,26 @@
+package com.meturial.domain.recipe.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class QueryChoiceRecipeListResponse {
+
+    private final UUID choiceId;
+
+    private final UUID recipeId;
+
+    private final String name;
+
+    private final String recipeImageUrl;
+
+    private final List<String> recipeCategory;
+
+    private final Float starRating;
+
+    private final Long starCount;
+}
