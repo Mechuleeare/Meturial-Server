5a61d7225aa076b1c1d6230ab71959440680d8a7
soyeonkim0227 <soyeonkim0227@naver.com>
2023-11-13 09:49:11 +0900

    💡 :: NotNull로 변경

--- src/main/java/com/meturial/domain/menu/presentation/dto/request/CreateMenuRequest.java
+++ src/main/java/com/meturial/domain/menu/presentation/dto/request/CreateMenuRequest.java
@@ -2,7 +2,6 @@ package com.meturial.domain.menu.presentation.dto.request;
 
 import com.meturial.domain.menu.domain.type.MenuType;
 import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +17,7 @@ public class CreateMenuRequest {
     @NotNull(message = "YYYY-MM-DD 형식으로 날짜를 입력하여 주세요.")
     private LocalDate date;
 
-    @NotBlank(message = "BREAKFAST(아침), LUNCH(점심), DINNER(저녁) 중 선택하여 주세요.")
+    @NotNull(message = "BREAKFAST(아침), LUNCH(점심), DINNER(저녁) 중 선택하여 주세요.")
     private MenuType menuType;
 
     @NotNull(message = "찜한 레시피 아이디를 입력하여 주세요.")
