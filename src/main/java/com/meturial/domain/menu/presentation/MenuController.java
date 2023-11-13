493ee78a6a46547b27b0ccfda53ebbd83a4d7a4c
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-14 08:53:54 +0900

    🌼 :: 식단 목록 조회 API 추가

--- src/main/java/com/meturial/domain/menu/presentation/MenuController.java
+++ src/main/java/com/meturial/domain/menu/presentation/MenuController.java
@@ -3,11 +3,21 @@ package com.meturial.domain.menu.presentation;
 import com.meturial.domain.menu.presentation.dto.request.CreateMenuRequest;
 import com.meturial.domain.menu.presentation.dto.request.UpdateMenuRequest;
 import com.meturial.domain.menu.presentation.dto.response.QueryMenuDetailResponse;
+import com.meturial.domain.menu.presentation.dto.response.QueryMenuListResponse;
 import com.meturial.domain.menu.service.MenuService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -41,5 +51,13 @@ public class MenuController {
     @GetMapping
     public QueryMenuDetailResponse queryMenuDetailByDate(@RequestParam("date") LocalDate date) {
         return menuService.queryMenuDetailByDate(date);
+    }
+
+    @GetMapping("/list")
+    public QueryMenuListResponse queryMenuListByYearAndMonth(
+            @RequestParam("year") Integer year,
+            @RequestParam("month") Integer month
+    ) {
+        return menuService.queryMenuListByYearAndMonth(year, month);
     }
 }
