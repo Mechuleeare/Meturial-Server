bdf0494dbb19036b172b0d3fc280a264aa52c705
soyeonkim0227 <soyeonkim0227@naver.com>
2023-11-13 09:41:42 +0900

    💡 :: Dto validation 어노테이션 추가

--- src/main/java/com/meturial/domain/menu/presentation/dto/request/CreateMenuRequest.java
+++ src/main/java/com/meturial/domain/menu/presentation/dto/request/CreateMenuRequest.java
@@ -1,6 +1,9 @@
 package com.meturial.domain.menu.presentation.dto.request;
 
 import com.meturial.domain.menu.domain.type.MenuType;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,8 +14,16 @@ import java.util.UUID;
 @NoArgsConstructor
 public class CreateMenuRequest {
 
+    @FutureOrPresent
+    @NotNull(message = "YYYY-MM-DD 형식으로 날짜를 입력하여 주세요.")
     private LocalDate date;
+
+    @NotBlank(message = "BREAKFAST(아침), LUNCH(점심), DINNER(저녁) 중 선택하여 주세요.")
     private MenuType menuType;
+
+    @NotNull(message = "찜한 레시피 아이디를 입력하여 주세요.")
     private UUID choiceRecipeId;
+
+    @NotNull(message = "푸쉬 알람 설정 여부를 선택하여 주세요.")
     private Boolean isActivated;
 }
