283451dac4bdda983d4e214df38c5b473579053c
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-14 23:37:51 +0900

    🌼 :: MenuType이랑 날짜로 UserIdList 가져오는 메소드 추가

--- src/main/java/com/meturial/domain/menu/domain/repository/CustomMenuRepository.java
+++ src/main/java/com/meturial/domain/menu/domain/repository/CustomMenuRepository.java
@@ -1,6 +1,7 @@
 package com.meturial.domain.menu.domain.repository;
 
 import com.meturial.domain.menu.domain.Menu;
+import com.meturial.domain.menu.domain.type.MenuType;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,4 +11,6 @@ public interface CustomMenuRepository {
     List<Menu> findAllByDateAndUserId(LocalDate date, UUID userId);
 
     List<Menu> findAllByBetweenCurrentMonthAndNextMonthAndUserId(LocalDate targetDate, UUID userId);
+
+    List<UUID> findAllUserIdByMenuTypeAndDate(MenuType menuType, LocalDate date);
 }
