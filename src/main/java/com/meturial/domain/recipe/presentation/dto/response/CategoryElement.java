95720c3cac58d7871278b907408d30b0ef0b8184
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-15 16:15:15 +0900

    💡 :: build메소드 dto에서 static메소드로 리팩토링

--- src/main/java/com/meturial/domain/recipe/presentation/dto/response/CategoryElement.java
+++ src/main/java/com/meturial/domain/recipe/presentation/dto/response/CategoryElement.java
@@ -1,5 +1,6 @@
 package com.meturial.domain.recipe.presentation.dto.response;
 
+import com.meturial.domain.recipe.domain.Category;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,4 +9,11 @@ import lombok.Getter;
 public class CategoryElement {
     private final String categoryName;
     private final String categoryImageUrl;
+
+    public static CategoryElement of(Category category) {
+        return CategoryElement.builder()
+                .categoryName(category.getName())
+                .categoryImageUrl(category.getCategoryImageUrl())
+                .build();
+    }
 }
