649c0fa2530a4363b513310115b08e7ae33111f9
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-15 19:53:10 +0900

    🌼 :: findAll 메소드 추가

--- src/main/java/com/meturial/domain/recipe/domain/repository/RecipeRepository.java
+++ src/main/java/com/meturial/domain/recipe/domain/repository/RecipeRepository.java
@@ -3,7 +3,10 @@ package com.meturial.domain.recipe.domain.repository;
 import com.meturial.domain.recipe.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RecipeRepository extends CrudRepository<Recipe, UUID>, CustomRecipeRepository {
+
+    List<Recipe> findAll();
 }
