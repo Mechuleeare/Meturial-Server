829defc546102845e93eb41360583e12a8fa3b24
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-19 16:01:34 +0900

    🔀 :: BaseUUID strategy 추가

--- src/main/java/com/meturial/global/entity/BaseUUIDEntity.java
+++ src/main/java/com/meturial/global/entity/BaseUUIDEntity.java
@@ -22,7 +22,7 @@ public abstract class BaseUUIDEntity {
 
     @Id
     @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)", nullable = false)
     private UUID id = UUID.randomUUID();
 }
