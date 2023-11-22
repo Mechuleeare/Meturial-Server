0cf1f9f0aa78b54da87e6605e3bf4c55b3b8f1f4
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-22 15:33:46 +0900

    🔀 :: Fcm Value로 변경

--- src/main/java/com/meturial/infra/fcm/FcmConfig.java
+++ src/main/java/com/meturial/infra/fcm/FcmConfig.java
@@ -5,23 +5,23 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Component
 public class FcmConfig {
 
-    @Value("${firebase.path}")
-    private String path;
+    @Value("${fcm.value}")
+    private String value;
 
     @PostConstruct
     public void initialize() {
         try {
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(FirebaseOptions.builder()
-                        .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(path).getInputStream()))
+                        .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(value.getBytes())))
                         .build());
             }
         } catch (IOException e) {
